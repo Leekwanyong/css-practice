80fa942e327988a1fd17740ce2d63e74b5179a2f
Leekwanyong <dlrkdid12@gmail.com>
2025-07-15 19:22:36 +0900

    ✨ feat: home section3 css 구현

---
+++ src/constance/homeIcon.ts
@@ -0,0 +1,11 @@
+export const LOGO_ICONS = [
+    { id: 1, icon: "/asset/icon/Frame 9.svg" },
+    { id: 1, icon: "/asset/icon/image9.svg" },
+    { id: 1, icon: "/asset/icon/image10.svg" },
+    { id: 1, icon: "/asset/icon/image11.svg" },
+    { id: 1, icon: "/asset/icon/image12.svg" },
+    { id: 1, icon: "/asset/icon/image13.svg" },
+    { id: 1, icon: "/asset/icon/image14.svg" },
+    { id: 1, icon: "/asset/icon/image15.svg" },
+    { id: 1, icon: "/asset/icon/image16.svg" },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import Title from "../components/common/Text/Title";
+import { LOGO_ICONS } from "../constance/homeIcon";
 import ProfileSection from "./ProfileSection";
 import WhatIDoSection from "./WhatIDoSection";
 
@@ -6,6 +8,18 @@ function Home() {
         <section>
             <ProfileSection />
             <WhatIDoSection />
+            <article className="mt-40">
+                <div>
+                    <Title>Where to find 🔍 my work?</Title>
+                    <ul className="mt-16 grid grid-cols-4 gap-8">
+                        {LOGO_ICONS.map((logo) => (
+                            <li key={logo.icon}>
+                                <img src={logo.icon} alt={logo.icon} className="object-cover" />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </article>
         </section>
     );
 }
