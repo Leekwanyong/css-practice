51c6a3ba0bba4cbbc45d86ecd59346f897028805
Leekwanyong <dlrkdid12@gmail.com>
2025-07-28 16:00:51 +0900

    ✨ feat: WhereToFind 반응형 구현

--- src/pages/home/WhereToFind.tsx
+++ src/pages/home/WhereToFind.tsx
@@ -3,13 +3,13 @@ import { LOGO_ICONS } from "../../constance/homeIcon";
 
 function WhereToFind() {
     return (
-        <article className="mt-40">
+        <article className="mt-40 px-4 lg:px-0">
             <div>
                 <Title>Where to find 🔍 my work?</Title>
-                <ul className="mt-16 grid grid-cols-4 gap-8">
+                <ul className="mt-16 grid grid-cols-1 px-4 lg:grid-cols-4 gap-8">
                     {LOGO_ICONS.map((logo) => (
                         <li key={logo.icon}>
-                            <img src={logo.icon} alt={logo.icon} className="object-cover" />
+                            <img src={logo.icon} alt={logo.icon} className="object-cover " />
                         </li>
                     ))}
                 </ul>
